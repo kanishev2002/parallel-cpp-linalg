9a5740a13863c00815aeabc014044468ab1d39f9
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-16 15:21:16 +0300

    Fixed operator ==

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -4,31 +4,34 @@
 #include <thread>
 
 template <typename T>
-bool Matrix<T>::operator==(const Matrix<T> &other) const {
-    if (shape() != other.shape()) {
-        return false;
-    }
+bool Matrix<T>::operator==(const Matrix<T>& other) const {
+  if (shape() != other.shape()) {
+    return false;
+  }
 
-    std::atomic<bool> equal = true;
-    auto compare_vectors = [&](const auto& row1, const auto& row2) {
-        for (size_t i = 0; i < row1.size(); ++i) {
-            if (!equal.exchange(row1[i] == row2[i])) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    std::vector<std::thread> threads;
-    for (size_t i = 0; i < shape().first; ++i) {
-        threads.emplace_back([&]() { equal.store(equal.load() && compare_vectors); }, matrix_[i], other.matrix_[i]);
+  std::atomic<bool> equal = true;
+  auto compare_vectors = [&](const auto& row1, const auto& row2) {
+    if (!equal.load()) {
+      return;
     }
+    equal.store(row1 == row2);
+  };
+
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < shape().first; ++i) {
+    threads.emplace_back(
+        [&, i]() { compare_vectors(matrix_[i], other.matrix_[i]); });
     for (auto& thr : threads) {
-        thr.join();
+      thr.join();
+      if (equal.load() == false) {
+        return false;
+      }
     }
+    return true;
+  }
 }
 
 template <typename T>
-bool Matrix<T>::operator!=(const Matrix<T> &other) const {
-    return !(*this == other);
+bool Matrix<T>::operator!=(const Matrix<T>& other) const {
+  return !(*this == other);
 }
--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "Matrix.cpp"
+#include "Matrix.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  Matrix<int> matrix({{1, 2}, {3, 4}});
+  std::cout << (matrix == matrix) << '\n';
+  return 0;
 }
