97beb9ce70cfffeeed1abd1e78106a2112f16a5e
Ilya Kanishev <kanishev2002@gmail.com>
2021-07-17 14:07:04 +0300

    Fixed print

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,58 +1,60 @@
 #include "Matrix.h"
 
 #include <atomic>
-#include <thread>
 #include <iostream>
+#include <thread>
 
-template<typename T>
+template <typename T>
 Matrix<T>::Matrix(std::vector<std::vector<T>> matrix) {
-    matrix_ = matrix;
+  matrix_ = matrix;
 }
 
+template <typename T>
+bool Matrix<T>::operator==(const Matrix<T>& other) const {
+  if (shape() != other.shape()) {
+    return false;
+  }
 
-template<typename T>
-bool Matrix<T>::operator==(const Matrix<T> &other) const {
-    if (shape() != other.shape()) {
+  std::atomic<bool> equal = true;
+  auto compare_vectors = [&](const auto& row1, const auto& row2) {
+    for (size_t i = 0; i < row1.size(); ++i) {
+      if (!equal.exchange(row1[i] == row2[i])) {
         return false;
+      }
     }
+    return true;
+  };
 
-    std::atomic<bool> equal = true;
-    auto compare_vectors = [&](const auto &row1, const auto &row2) {
-        for (size_t i = 0; i < row1.size(); ++i) {
-            if (!equal.exchange(row1[i] == row2[i])) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    std::vector<std::thread> threads;
-    for (size_t i = 0; i < shape().first; ++i) {
-        threads.emplace_back([&]() { equal.store(equal.load() && compare_vectors); }, matrix_[i], other.matrix_[i]);
-    }
-    for (auto &thr : threads) {
-        thr.join();
-    }
-
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < shape().first; ++i) {
+    threads.emplace_back(
+        [&]() { equal.store(equal.load() && compare_vectors); }, matrix_[i],
+        other.matrix_[i]);
+  }
+  for (auto& thr : threads) {
+    thr.join();
+  }
 }
 
-template<typename T>
-bool Matrix<T>::operator!=(const Matrix<T> &other) const {
-    return !(*this == other);
+template <typename T>
+bool Matrix<T>::operator!=(const Matrix<T>& other) const {
+  return !(*this == other);
 }
 
-template<typename T>
-void Matrix<T>::print(std::ostream &out, char column_spliter, char row_spliter) const {
-    for (size_t i = 0; i < matrix_.size(); ++i) {
-        for (size_t j = 0; j < matrix_[0].size(); ++j) {
-            out << matrix_[i][j] << '\t';
-        }
-        out << '\n';
+template <typename T>
+void Matrix<T>::print(std::ostream& out, char column_separator,
+                      char row_separator) const {
+  const auto [rows, columns] = shape();
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < columns; ++j) {
+      out << matrix_[i][j] << column_separator;
     }
+    out << row_separator;
+  }
 }
 
-template<typename T>
-std::ostream &operator<<(std::ostream &out, const Matrix<T> &mat) {
-    mat.print(out,'\t','\n');
-    return out;
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Matrix<T>& mat) {
+  mat.print(out, '\t', '\n');
+  return out;
 }
--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -3,28 +3,27 @@
 
 template <typename T>
 class Matrix {
-private:
-    std::vector<std::vector<T>> matrix_;
+ private:
+  std::vector<std::vector<T>> matrix_;
 
-public:
-    Matrix() = default;
-    Matrix(size_t rows, size_t columns);
-    Matrix(std::vector<std::vector<T>> matrix);
-    Matrix(const Matrix<T>& other);
-    Matrix(Matrix<T>&& other);
+ public:
+  Matrix() = default;
+  Matrix(size_t rows, size_t columns);
+  Matrix(std::vector<std::vector<T>> matrix);
+  Matrix(const Matrix<T>& other);
+  Matrix(Matrix<T>&& other);
 
+  bool operator==(const Matrix<T>& other) const;
+  bool operator!=(const Matrix<T>& other) const;
 
-    bool operator==(const Matrix<T>& other) const;
-    bool operator!=(const Matrix<T>& other) const;
+  void print(std::ostream& out, char column_spliter = '\t',
+             char row_spliter = '\n') const;
 
-    void print(std::ostream &out, char column_spliter, char row_spliter) const;
+  Matrix<T> operator+(const Matrix<T>& other) const;
+  Matrix<T> operator-(const Matrix<T>& other) const;
+  Matrix<T> operator*(const Matrix<T>& other) const;
 
+  Matrix<T> operator[](size_t index);
 
-    Matrix<T> operator+(const Matrix<T>& other) const;
-    Matrix<T> operator-(const Matrix<T>& other) const;
-    Matrix<T> operator*(const Matrix<T>& other) const;
-
-    Matrix<T> operator[](size_t index);
-
-    std::pair<size_t, size_t> shape();
+  std::pair<size_t, size_t> shape() const;
 };
