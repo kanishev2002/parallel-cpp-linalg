f5f9e3cab3a90f2fb725d414a85361913690507b
InfinityZxc <86811456+InfinityZxc@users.noreply.github.com>
2021-07-20 21:04:19 +0300

    transpose

---
+++ include/Transpose.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Matrix.h"
+
+template <typename T>
+Matrix<T> transpose(const Matrix<T>& matrix);
---
+++ src/Transpose/Transpose.cpp
@@ -0,0 +1,19 @@
+#include "../../include/dot.h"
+
+#include <shared_mutex>
+#include <vector>
+
+template <typename T>
+Matrix<T> transpose(const Matrix<T>& matrix) {
+  std::shared_lock<T>(matrix.shared_mtx_)
+  const auto[rows, columns] = matrix.shape();
+  std::vector<std::vector<T>> res_data(columns, std::vector<T>(rows));
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < columns; ++i) {
+      res_data[j][i] = matrix[i][j]
+    }
+  }
+
+  return Matrix<T>(res_data);
+}
+
