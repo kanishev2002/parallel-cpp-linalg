c64cf6910e11e6fc0df16736ff4a6d086caf09fa
0masiza0 <rlar7007@gmail.com>
2021-07-21 13:26:39 +0300

    Add shared lock

--- src/matrix_power/matrix_power.cpp
+++ src/matrix_power/matrix_power.cpp
@@ -1,9 +1,11 @@
 #include "../../include/matrix_power.h"
 #include "../../include/dot.h"
 #include <cmath>
+#include <shared_mutex>
 
 template<typename T>
 Matrix<T> matrix_power(const Matrix<T>& a, size_t n) {
+  std::shared_lock<Matrix<T>>(a.shared_mtx_);
   if (a.shape().first != a.shape().second) {
     throw std::invalid_argument("Matrix should be square");
   }
