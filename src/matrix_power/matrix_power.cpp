25baab10a1112c76af461351e2862be7ddbbccca
0masiza0 <rlar7007@gmail.com>
2021-07-21 13:20:41 +0300

    Rewrite Matrix power

--- src/matrix_power/matrix_power.cpp
+++ src/matrix_power/matrix_power.cpp
@@ -1,4 +1,5 @@
 #include "../../include/matrix_power.h"
+#include "../../include/dot.h"
 #include <cmath>
 
 template<typename T>
@@ -6,30 +7,16 @@ Matrix<T> matrix_power(const Matrix<T>& a, size_t n) {
   if (a.shape().first != a.shape().second) {
     throw std::invalid_argument("Matrix should be square");
   }
-  if (n == 0) {
-    return Matrix<T>::eye(a.shape().first);
-  }
-  auto result = a;
-  auto split_into_sum_of_pows = [&](size_t n_) {
-    std::vector<size_t> powers;
-    size_t i = 1;
-    while (i <= n_) {
-      if (n_ & i) {
-        powers.push_back(i);
-      }
-      i = i << 1;
-    }
-    return powers;
-  };
-  auto powers = split_into_sum_of_pows(n);
+  auto result = Matrix<T>::eye(a.shape().first);
   auto tmp = a;
-  size_t tmp_pow = 1;
-  for (auto it = powers.begin(); it != powers.end(); ++it) {
-    while (tmp_pow != *it) {
+  while (n > 0) {
+    if (n & 1) {
+      result = dot(result, tmp);
+      --n;
+    } else {
       tmp = dot(tmp, tmp);
-      tmp_pow *= 2;
+      n >>= 1;
     }
-    result = dot(result, tmp);
   }
   return result;
 }
