feb5a9d3d2555dc236270ca3196da8139f5394a9
Ilya Kanishev <kanishev2002@gmail.com>
2021-08-25 19:04:59 +0300

    Updated Transpose to use ThreadPool

--- src/Transpose/transpose.cpp
+++ src/Transpose/transpose.cpp
@@ -1,6 +1,8 @@
+#include <include/thread_pool.h>
 #include <include/transpose.h>
 
 #include <shared_mutex>
+#include <src/thread_pool/thread_pool.cpp>
 #include <vector>
 
 template <typename T>
@@ -11,17 +13,17 @@ Matrix<T> Transpose(const Matrix<T>& matrix) {
   size_t columns = mat_columns;
   std::vector<std::thread> threads;
   std::vector<std::vector<T>> res_data(columns);
-  for (size_t old_col = 0; old_col < columns; ++old_col) {
-    threads.emplace_back([&, old_col, rows] {
-      std::vector<T> new_row(rows);
-      for (size_t old_row = 0; old_row < rows; ++old_row) {
-        new_row[old_row] = matrix[old_row][old_col];
-      }
-      res_data[old_col] = std::move(new_row);
-    });
-  }
-  for (auto& thread : threads) {
-    thread.join();
+  {
+    ThreadPool pool;
+    for (size_t old_col = 0; old_col < columns; ++old_col) {
+      pool.enqueue_task([&, old_col, rows] {
+        std::vector<T> new_row(rows);
+        for (size_t old_row = 0; old_row < rows; ++old_row) {
+          new_row[old_row] = matrix[old_row][old_col];
+        }
+        res_data[old_col] = std::move(new_row);
+      });
+    }
   }
   return Matrix<T>(std::move(res_data));
 }
