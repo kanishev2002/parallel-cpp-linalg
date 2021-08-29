2071d86149a85a8d2ebce7e77aa401334be572be
Ilya Kanishev <kanishev2002@gmail.com>
2021-08-29 18:43:22 +0300

    Add performance testing

--- tests/test_solve.cpp
+++ tests/test_solve.cpp
@@ -1,9 +1,13 @@
 #include <gtest/gtest.h>
 #include <include/matrix.h>
 #include <include/solve.h>
+#include <include/solve_singular.h>
 
+#include <chrono>
+#include <random>
 #include <src/matrix/matrix.cpp>
 #include <src/solve/solve.cpp>
+#include <src/solve/solve_singular.cpp>
 
 TEST(Solve, Solve_simple) {
   auto A = Matrix<double>(
@@ -32,4 +36,31 @@ TEST(Solve, No_solution) {
       std::vector<std::vector<double>>({{1, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
   auto B = Matrix<double>(std::vector<std::vector<double>>({{1}, {2}, {3}}));
   ASSERT_THROW(Solve(A, B), std::invalid_argument);
+}
+
+TEST(Solve, Compare_to_single_thread) {
+  std::ranlux24 random;
+  constexpr size_t matrix_size = 1000;
+  Matrix<double> A(matrix_size, matrix_size), B(matrix_size, 1);
+  for (size_t i = 0; i < matrix_size; ++i) {
+    for (size_t j = 0; j < matrix_size; ++j) {
+      A[i][j] = random();
+    }
+  }
+  for (size_t i = 0; i < matrix_size; ++i) {
+    B[i][0] = random();
+  }
+
+  auto mt_begin = std::chrono::high_resolution_clock::now();
+  Solve(A, B);
+  auto mt_end = std::chrono::high_resolution_clock::now();
+
+  auto st_begin = std::chrono::high_resolution_clock::now();
+  SolveSingular(A, B);
+  auto st_end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double> st_res(st_end - st_begin);
+  std::chrono::duration<double> mt_res(mt_end - mt_begin);
+
+  EXPECT_LE(mt_res.count(), st_res.count());
 }
